acdfbad4392c74c292c359197bb6f5697a326fd8
evanbones <74276616+evanbones@users.noreply.github.com>
2025-12-25 14:41:24 -0800

    Glimmer gravel effects

--- common/src/main/java/com/infernalstudios/infernalexp/block/GlimmerGravelBlock.java
+++ common/src/main/java/com/infernalstudios/infernalexp/block/GlimmerGravelBlock.java
@@ -2,11 +2,18 @@ package com.infernalstudios.infernalexp.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.particles.BlockParticleOption;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.FallingBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
 
 public class GlimmerGravelBlock extends FallingBlock {
     public GlimmerGravelBlock(Properties properties) {
@@ -14,15 +21,26 @@ public class GlimmerGravelBlock extends FallingBlock {
     }
 
     @Override
-    public BlockState updateShape(BlockState state, Direction direction, BlockState otherState, LevelAccessor accessor, BlockPos otherPos, BlockPos whatever) {
+    public @NotNull BlockState updateShape(@NotNull BlockState state, @NotNull Direction direction, @NotNull BlockState otherState, @NotNull LevelAccessor accessor, @NotNull BlockPos otherPos, @NotNull BlockPos whatever) {
         return state;
     }
 
     @Override
-    public void onPlace(BlockState state, Level level, BlockPos pos, BlockState old, boolean piston) {}
+    public void onPlace(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull BlockState old, boolean piston) {}
 
     @Override
-    public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
+    public void tick(@NotNull BlockState state, ServerLevel level, BlockPos pos, @NotNull RandomSource random) {
+        if (isFree(level.getBlockState(pos.below())) && pos.getY() >= level.getMinBuildHeight()) {
+            level.playSound(null, pos, SoundEvents.GRAVEL_BREAK, SoundSource.BLOCKS, 1.0F, 0.8F);
+            level.sendParticles(new BlockParticleOption(ParticleTypes.BLOCK, state),
+                    pos.getX() + 0.5D, pos.getY(), pos.getZ() + 0.5D,
+                    5, 0.25D, 0.25D, 0.25D, 0.05D);
+        }
+        super.tick(state, level, pos, random);
+    }
+
+    @Override
+    public void stepOn(@NotNull Level level, @NotNull BlockPos pos, @NotNull BlockState state, @NotNull Entity entity) {
         super.stepOn(level, pos, state, entity);
         if (!entity.isSteppingCarefully())
             level.scheduleTick(pos, this, this.getDelayAfterPlace());
